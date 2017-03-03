4a96c31868e729e4b4ad196037f527fc9d09d0e7
tzw1990 <timwongbrooklyn@gmail.com>
2017-03-02 21:34:16 -0500

    update with jhipster

---
+++ src/main/java/com/askfolio/mainsite/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.askfolio.mainsite.config.audit;
---
+++ src/main/webapp/app/components/form/pagination.constants.js
@@ -0,0 +1,9 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('mainsiteApp')
+        .constant('paginationConstants', {
+            'itemsPerPage': 20
+        });
+})();
